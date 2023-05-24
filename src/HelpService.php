24d04c31ebef72279b8b1b7708064e7d71478835
hxdef <izotope777@gmail.com>
2023-05-24 15:55:53 +0600

    Issue #3362367: Remove drupal_get_path in bibcite

--- src/HelpService.php
+++ src/HelpService.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\bibcite;
 
+use Drupal\Core\Extension\ModuleExtensionList;
+use Drupal\Core\Language\LanguageManagerInterface;
+
 /**
  * Class HelpService.
  *
@@ -10,12 +13,37 @@ namespace Drupal\bibcite;
 class HelpService implements HelpServiceInterface {
 
   /**
+   * Extenstion list service.
+   *
+   * @var \Drupal\Core\Extension\ModuleExtensionList
+   */
+  protected ModuleExtensionList $extensionList;
+
+  /**
+   * Language manager service.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected LanguageManagerInterface $languageManager;
+
+  /**
+   * Constructs a HelpService object.
+   *
+   * @param \Drupal\Core\Extension\ModuleExtensionList $extension_list
+   *   Module extension list service
+   */
+  public function __construct(ModuleExtensionList $extension_list, LanguageManagerInterface $language_manager) {
+    $this->extensionList = $extension_list;
+    $this->languageManager = $language_manager;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function getHelpMarkup($links, $route, $module) {
-    $module_path = drupal_get_path('module', $module);
-    $lang = \Drupal::languageManager()->getCurrentLanguage()->getId();
-    $def = \Drupal::languageManager()->getDefaultLanguage()->getId();
+    $module_path = $this->extensionList->getPath($module);
+    $lang = $this->languageManager->getCurrentLanguage()->getId();
+    $def = $this->languageManager->getDefaultLanguage()->getId();
     $path = $module_path . '/help/' . $lang . '/' . $route . 'html';
     if ($def === $lang || !file_exists($path)) {
       $path = $module_path . '/help/default/' . $route . '.html';
