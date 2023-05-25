6e63bccfaeb1ab1397a7abff43a1eed6e2a162f3
hxdef <izotope777@gmail.com>
2023-05-25 15:19:41 +0600

    Issue #3362637: The theme 'stable' is deprecated in bibcite_entity

--- modules/bibcite_entity/tests/src/Functional/BibciteEntityTest.php
+++ modules/bibcite_entity/tests/src/Functional/BibciteEntityTest.php
@@ -22,7 +22,7 @@ class BibciteEntityTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  protected $defaultTheme = 'stable';
+  protected $defaultTheme = 'stark';
 
   /**
    * Test user.
